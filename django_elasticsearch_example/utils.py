52000031ac8a6bfc099a823d398072916f259f28
Patryk Mlynarek <p.mlynarek@sunscrapers.com>
2019-05-22 13:09:15 +0200

    Disable logging for function wait_elasticsearch_availability.

--- django_elasticsearch_example/utils.py
+++ django_elasticsearch_example/utils.py
@@ -1,12 +1,22 @@
+import logging
 from elasticsearch_dsl.connections import connections
+
+
+class DisableLogger():
+    def __enter__(self):
+        logging.disable(logging.CRITICAL)
+
+    def __exit__(self, a, b, c):
+        logging.disable(logging.NOTSET)
 
 
 def wait_elasticsearch_availability():
     good_statuses = ['green', 'yellow']
-    while True:
-        try:
-            respone = connections.get_connection().cluster.health()
-            if respone.get('status') in good_statuses:
-                return
-        except Exception:
-            pass
+    with DisableLogger():
+        while True:
+            try:
+                respone = connections.get_connection().cluster.health()
+                if respone.get('status') in good_statuses:
+                    return
+            except Exception:
+                pass
